1426d73a46e12b4e2a85231cd9f56cbfc238de61
Yousef El Sayed <115503073+youyes004@users.noreply.github.com>
2022-11-16 21:35:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <math.h>
+#include <math.h> 
+
 using namespace std;
 
 int main() {
 float a=0, b=0, x=0, y=0, z=0;
 cin>> a >> b >> x >> y;
 if(x<0 && y>0){
-z=a*x-by;
-cout<<z<<endl;
+z=a*x - by;
+cout<< z <<endl;
 } else if(x>=0 && y<=0){
-z=a*(pow(x, 2))-b*y
-cout<<z<<endl;
+z=a*(pow(x, 2)) - b*y
+cout<< z <<endl;
 }else{
-z=a*x+b*(pow(y, 2))
-cout<<z<<endl;
+z=a*x + b*(pow(y, 2))
+cout<< z <<endl;
 }
    return 0;
 }
