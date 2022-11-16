06fdde8e6875f22b121b75341063758374afe540
Yousef El Sayed <115503073+youyes004@users.noreply.github.com>
2022-11-16 21:32:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 int main() {
-float a, b, x, y, z;
+float a=0, b=0, x=0, y=0, z=0;
 cin>> a >> b >> x >> y;
-if(x<0 and y>0){
+if(x<0 && y>0){
 z=a*x-by;
 cout<<z<<endl;
-
-} else if(x>=0 and y<=0){
+} else if(x>=0 && y<=0){
 z=a*pow(x, 2)-b*y
 cout<<z<<endl;
 }else{
